06e824366a812076097be43f24bd4961c238cb79
yuki-takara <tkra.yuuki@gmail.com>
2021-02-16 02:00:07 +0900

    feat: 記事一覧/詳細を表示出来るようにする

--- pages/index.vue
+++ pages/index.vue
@@ -1,65 +1,21 @@
 <template>
-  <div class="container">
-    <div>
-      <Logo />
-      <h1 class="title">add-nuxt-content-sample</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+  <ul>
+    <li v-for="article in articles" :key="article.slug">
+      <nuxt-link :to="`/articles/${article.slug}`">
+        {{ article.title }}
+      </nuxt-link>
+      <p>{{ article.updatedAt }}</p>
+    </li>
+  </ul>
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import { Context } from '@nuxt/types'
 
-export default Vue.extend({})
+export default {
+  async asyncData({ $content, params }: Context) {
+    const articles = await $content('articles').fetch()
+    return { articles }
+  },
+}
 </script>
-
-<style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
-</style>
